36c92eab711eed2c79ca2f845823af11b9a4e4c2
Andrew614 <azhang213@gmail.com>
2019-07-29 11:48:50 -0400

    inital commit

---
+++ src/main/java/wcci/inlineconnect/InLineConnectApplication.java
@@ -0,0 +1,13 @@
+package wcci.inlineconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InLineConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InLineConnectApplication.class, args);
+	}
+
+}
