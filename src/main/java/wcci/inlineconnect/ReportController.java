34c86e82cf30a92a480f53c6afa34fb166b48e48
Sbsmith1101 <sbsmith1101@gmail.com>
2019-07-30 15:17:00 -0400

    added controller Test

--- src/main/java/wcci/inlineconnect/ReportController.java
+++ src/main/java/wcci/inlineconnect/ReportController.java
@@ -2,6 +2,9 @@ package wcci.inlineconnect;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,5 +18,15 @@ public class ReportController {
 	public Iterable<Report> findAllReports() {
 		return reportRepo.findAll();
 	}
+	
+	@GetMapping("reports/{id}")
+	public Report findSingleReports(@PathVariable Long id) {
+		return reportRepo.findById(id).get();
+	}
+	@PostMapping("reports")
+	public Iterable<Report> createReport(@RequestBody Report report){
+		reportRepo.save(report);
+		return reportRepo.findAll();
 		
+	}
 }
--- src/test/java/wcci/inlineconnect/WebLayerTest.java
+++ src/test/java/wcci/inlineconnect/WebLayerTest.java
@@ -1,7 +1,9 @@
 package wcci.inlineconnect;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -14,6 +16,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -51,11 +54,25 @@ public class WebLayerTest {
 	@Test
 	public void shouldReturnOneReport() throws Exception {
 		when(reportRepo.findById(1l)).thenReturn(Optional.of(report));
-		mockMvc.perform(get("/api/report/1"))
+		mockMvc.perform(get("/api/reports/1"))
 				.andExpect(status().isOk())
 				.andExpect(content().contentType("application/json;charset=UTF-8"))
-				.andExpect(content().json("[{}]"))
-				.andExpect(content().json(mapper.writeValueAsString(Collections.singletonList(report)), true));
+				.andExpect(content().json("{}"))
+				.andExpect(content().json(mapper.writeValueAsString(report)));
 
 	}
+	@Test
+	public void shouldAddRepot() throws Exception {
+		when(reportRepo.save(any(Report.class))).thenReturn(report);
+		when(reportRepo.findAll()).thenReturn(Collections.singletonList(report));
+		mockMvc.perform(post("/api/reports")
+				.contentType(MediaType.APPLICATION_JSON_UTF8)
+				 .content(mapper.writeValueAsString(report)))
+        .andExpect(status().isOk())
+         .andExpect(content().json(mapper.writeValueAsString(Collections.singletonList(report))));
+				
+				
+	}
+	
+	
 }
