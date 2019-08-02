4b8d67a63b7b2ab7badf40a12b8168c3bc9a695d
Sshipley12 <steph.shipley@yahoo.com>
2019-08-02 10:39:51 -0400

    populated initializer

--- src/main/java/wcci/inlineconnect/Initializer.java
+++ src/main/java/wcci/inlineconnect/Initializer.java
@@ -4,46 +4,35 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import wcci.inlineconnect.models.CardiacReport;
+import wcci.inlineconnect.models.MedicalReport;
+import wcci.inlineconnect.models.TraumaReport;
+import wcci.inlineconnect.repositories.CardiacReportRepository;
+import wcci.inlineconnect.repositories.MedicalReportRepository;
+import wcci.inlineconnect.repositories.TraumaReportRepository;
+
 @Component
 public class Initializer implements CommandLineRunner {
 
-//	@Autowired
-//	ReportRepository reportRepo;
+	@Autowired
+	MedicalReportRepository medicalReportRepo;
+	@Autowired
+	TraumaReportRepository traumaReportRepo;
+	@Autowired
+	CardiacReportRepository cardiacReportRepo;
 
 	@Override
 	public void run(String... args) throws Exception {
-//
-//		Report report1 = new Report("01", "MI",
-//				"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-//				"120/80", "126", "95", "16", "15", "80");
-//
-//		reportRepo.save(report1);
-//
-//		Report report2 = new Report("01", "MVC",
-//				"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-//				"140/90", "45", "99", "19", "10", "33");
-//
-//		reportRepo.save(report2);
-//
-//		Report report3 = new Report("01", "Fall",
-//				"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-//				"88/50", "66", "97", "21", "5", "73");
-//
-//		reportRepo.save(report3);
-//
-//		Report report4 = new Report("01", "lll",
-//				"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-//				"121/82", "94", "96", "32", "7", "80");
-//
-//		reportRepo.save(report4);
-//
-//		Report report5 = new Report("01", "kpkp",
-//				"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-//				"117/76", "54", "100", "4", "13", "76");
-//
-//		reportRepo.save(report5);
-//
-//		System.out.println("RUNNING INITIALIZER");
+
+		MedicalReport report1 = new MedicalReport("51", "CVA", "lmlmlmlmlmlml", "120/60", "60", "98%", "16", "15", "80",
+				"Sinus Rhythm", "april");
+		medicalReportRepo.save(report1);
+		TraumaReport report2 = new TraumaReport("50", "MI", "lmlmlmlmlmlml", "128/60", "69", "90%", "8", "10", "90",
+				"SVT");
+		traumaReportRepo.save(report2);
+		CardiacReport report4 = new CardiacReport("Witness", "lmlmlmlmlmlml", "SVT", "", "");
+		cardiacReportRepo.save(report4);
+		System.out.println("RUNNING INITIALIZER");
 
 	}
 }
--- src/main/java/wcci/inlineconnect/models/MedicalReport.java
+++ src/main/java/wcci/inlineconnect/models/MedicalReport.java
@@ -1,123 +1,62 @@
 package wcci.inlineconnect.models;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Lob;
-
 
 @Entity
 public class MedicalReport extends Report {
-	
 
-		@Id
-		@GeneratedValue
-		private Long id;
+	private String bloodPressure;
+	private String heartRate;
+	private String spO2;
+	private String respiratoryRate;
+	private String GCS;
+	private String bloodSugar;
+	private String rhythm;
 
-		private String medicNum;
-		private String chiefComplaint;
-		
-		@Lob // may need to be BLOB for voice to text
-		private String narrative;
+	public MedicalReport(String medicNum, String chiefComplaint, String narrative, String bloodPressure,
+			String heartRate, String spO2, String respiratoryRate, String GCS, String bloodSugar, String rhythm, String date) {
+		super(medicNum, chiefComplaint, date,narrative);
+		this.bloodPressure = bloodPressure;
+		this.heartRate = heartRate;
+		this.spO2 = spO2;
+		this.respiratoryRate = respiratoryRate;
+		this.GCS = GCS;
+		this.bloodSugar = bloodSugar;
+		this.rhythm = rhythm;
+	}
 
-		private String bloodPressure;
-		private String heartRate;
-		private String spO2;
-		private String respiratoryRate;
-		private String GCS;
-		private String bloodSugar;
-		private String cardiacFuction;
-		
-
-		public MedicalReport(String medicNum, String chiefComplaint, String narrative, String bloodPressure, String heartRate,
-				String spO2, String respiratoryRate, String GCS, String bloodSugar, String cardiacFuction) {
-			this.medicNum = medicNum;
-			this.chiefComplaint = chiefComplaint;
-			this.narrative = narrative;
-			this.bloodPressure = bloodPressure;
-			this.heartRate = heartRate;
-			this.spO2 = spO2;
-			this.respiratoryRate = respiratoryRate;
-			this.GCS = GCS;
-			this.bloodSugar = bloodSugar;
-			this.cardiacFuction = cardiacFuction;
-		}
-		
-		public MedicalReport() {
-			
-		}
-
-		public Long getId() {
-			return id;
-		}
-
-		public String getMedicNum() {
-			return medicNum;
-		}
-
-		public String getChiefComplaint() {
-			return chiefComplaint;
-		}
-
-	
-
-		public String getNarrative() {
-			return narrative;
-		}
-
-		public String getBloodPressure() {
-			return bloodPressure;
-		}
-
-		public String getHeartRate() {
-			return heartRate;
-		}
-
-		public String getSpO2() {
-			return spO2;
-		}
-
-		public String getRespiratoryRate() {
-			return respiratoryRate;
-		}
-
-		public String getGCS() {
-			return GCS;
-		}
-
-		public String getBloodSugar() {
-			return bloodSugar;
-		}
-		public String getCardiacFuction() {
-			return cardiacFuction;
-
-		
+	public MedicalReport() {
 
 	}
 
-		@Override
-		public int hashCode() {
-			final int prime = 31;
-			int result = 1;
-			result = prime * result + ((id == null) ? 0 : id.hashCode());
-			return result;
-		}
+	
 
-		@Override
-		public boolean equals(Object obj) {
-			if (this == obj)
-				return true;
-			if (obj == null)
-				return false;
-			if (getClass() != obj.getClass())
-				return false;
-			MedicalReport other = (MedicalReport) obj;
-			if (id == null) {
-				if (other.id != null)
-					return false;
-			} else if (!id.equals(other.id))
-				return false;
-			return true;
-		}
+	public String getBloodPressure() {
+		return bloodPressure;
+	}
+
+	public String getHeartRate() {
+		return heartRate;
+	}
+
+	public String getSpO2() {
+		return spO2;
+	}
+
+	public String getRespiratoryRate() {
+		return respiratoryRate;
+	}
+
+	public String getGCS() {
+		return GCS;
+	}
+
+	public String getBloodSugar() {
+		return bloodSugar;
+	}
+
+	public String getRhythm() {
+		return rhythm;
+	}
 
 }
