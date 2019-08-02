b2494a1f7b9cd4100e83e74a586c775d2641da05
Andrew614 <azhang213@gmail.com>
2019-08-02 14:08:57 -0400

    add jpa tests

--- src/main/java/wcci/inlineconnect/repositories/CardiacReportRepository.java
+++ src/main/java/wcci/inlineconnect/repositories/CardiacReportRepository.java
@@ -1,9 +1,10 @@
 package wcci.inlineconnect.repositories;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import wcci.inlineconnect.models.CardiacReport;
-
+@Repository
 public interface CardiacReportRepository extends CrudRepository<CardiacReport, Long> {
 
 }
