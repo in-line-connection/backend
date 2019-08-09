aaa7d68d22dabfca9b7c8f571848310a54034073
Sshipley12 <steph.shipley@yahoo.com>
2019-08-09 11:53:55 -0400

    creation of other complaint report

---
+++ src/main/java/wcci/inlineconnect/models/OtherComplaintReport.java
@@ -0,0 +1,37 @@
+package wcci.inlineconnect.models;
+
+public class OtherComplaintReport extends TraumaReport {
+
+	private boolean penetratingTrauma;
+	private boolean bluntForceTrauma;
+	private String incident;
+	
+	
+
+	public OtherComplaintReport(boolean penetratingTrauma, boolean bluntForceTrauma, String incident, String medicNum, String narrative, String sex, String age, String date,
+			String bloodPressure, String heartRate, String spO2, String respiratoryRate, String gcs,
+			String bloodSugar) {
+		super(medicNum, narrative, sex, age, date, bloodPressure, heartRate, spO2, respiratoryRate, gcs, bloodSugar);
+		
+	}
+
+	public OtherComplaintReport() {
+		super();
+	}
+
+	public boolean isPenetratingTrauma() {
+		return penetratingTrauma;
+	}
+
+	public boolean isBluntForceTrauma() {
+		return bluntForceTrauma;
+	}
+
+	public String getIncident() {
+		return incident;
+	}
+	
+	
+	
+	
+}
