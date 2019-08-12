3abc3f71968f88222440798b5db764f19853fb25
isaac-canonico <canonico.isaac@gmail.com>
2019-08-12 11:10:29 -0400

    checked endpoints for post request to work

--- src/main/java/wcci/inlineconnect/controllers/FallReportController.java
+++ src/main/java/wcci/inlineconnect/controllers/FallReportController.java
@@ -1,9 +1,16 @@
 package wcci.inlineconnect.controllers;
 
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,4 +38,10 @@ public class FallReportController {
 		return fallReportRepo.findById(id).get();
 	}
 
+	@PostMapping({ "", "/" }) 
+	public void createTruamaReport(@RequestBody FallReport reportToSave, HttpServletResponse response)
+			throws JSONException, IOException {
+		fallReportRepo.save(reportToSave);
+		response.sendRedirect("/api/fall-reports");
+	}
 }
