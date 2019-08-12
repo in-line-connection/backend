58a9a393438adaa2f661822712fdc5ed0ec98450
isaac-canonico <canonico.isaac@gmail.com>
2019-08-12 13:15:37 -0400

    started initializer

--- src/main/java/wcci/inlineconnect/Initializer.java
+++ src/main/java/wcci/inlineconnect/Initializer.java
@@ -3,11 +3,15 @@ package wcci.inlineconnect;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import wcci.inlineconnect.models.FallReport;
+
 @Component
 public class Initializer implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+		
+		FallReport report1 = new FallReport(false, false, "", false, false, null, null, null, null, null, null, null, null, null, null, null, null);
 
 		System.out.println("RUNNING INITIALIZER");
 
