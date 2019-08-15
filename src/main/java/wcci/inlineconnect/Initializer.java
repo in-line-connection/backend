5f6310b1c1afc7cd58afc652eb8fdda88bd544e4
Sshipley12 <steph.shipley@yahoo.com>
2019-08-15 10:00:18 -0400

    minor changes to one report

--- src/main/java/wcci/inlineconnect/Initializer.java
+++ src/main/java/wcci/inlineconnect/Initializer.java
@@ -30,14 +30,14 @@ public class Initializer implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		
 
-		FallReport report1 = new FallReport(false, false, "standing to ground", true, false, "medic 51 LCEMS","08-15-2019" , "1520","Medic 51 LCEMS disptached to walmart of Ironton for 79 y/o M/ patient.Patient was standing at checkout and tripped over his cane. Patient fell to ground stricking his arm on the counter. Upon arrival patient was alertx4 and denied LOC. Patients vitals obtained, within normal limites. Pupils pearrl and equal. GSC normal, range of motion present, PMS presentx4. Patients arm without injury. No pertent medical hx. Patient denied treatment/ transport on scene. Patient stable and alert to all standing. Refusel signed and call released at 1549. Medic 51 back in service", "Male", "79", "145/65", "75", "98%", "18", "15", "75");
+		FallReport report1 = new FallReport(false, false, "standing to ground", true, false, "Medic 51 LCEMS","08-15-2019" , "1520","Medic 51 LCEMS disptached to walmart of Ironton for 79 y/o M/ patient.Patient was standing at checkout and tripped over his cane. Patient fell to ground stricking his arm on the counter. Upon arrival patient was alertx4 and denied LOC. Patients vitals obtained, within normal limites. Pupils pearrl and equal. GSC normal, range of motion present, PMS presentx4. Patients arm without injury. No pertent medical hx. Patient denied treatment/ transport on scene. Patient stable and alert to all standing. Refusel signed and call released at 1549. Medic 51 back in service", "Male", "79", "145/65", "75", "98%", "18", "15", "75");
 		fallRepo.save(report1);
 		
-		MotorVehicleCrashReport report2 = new MotorVehicleCrashReport("medic 21 LCEMS", "07-11-2018", "1610", "MVC on county rd 1 mile marker 3. Car slide off the road", "F",
+		MotorVehicleCrashReport report2 = new MotorVehicleCrashReport("Medic 21 LCEMS", "07-11-2018", "1610", "MVC on county rd 1 mile marker 3. Car slide off the road", "F",
 				"20", "110/64", "69", "100%", "16", "15",
 				"n/a", "driver", "65", true, false, false);
 		mvcRepo.save(report2);
-		GunShotWoundReport report3 = new GunShotWoundReport("medic 21 LCEMS", "08-01-2019", "1006", "Medic 21 dispatched to residence for GSW victim. 21 year old female patient. GSW to upper right forearm", "F",
+		GunShotWoundReport report3 = new GunShotWoundReport("Medic 21 LCEMS", "08-01-2019", "1006", "Medic 21 dispatched to residence for GSW victim. 21 year old female patient. GSW to upper right forearm", "F",
 			"21", "120/60", "70", "99", "19", "15",
 			"n/a", "right forearm", "1", true, "glock", false);
 		gswRepo.save(report3);
@@ -45,9 +45,9 @@ public class Initializer implements CommandLineRunner {
 				"80", "99%","20", "12", "30");
 		otherRepo.save(report4);
 		
-		GunShotWoundReport report6 = new GunShotWoundReport("medic 11 LCEMS", "09-01-2019", "1006", "Medic 21 dispatched to residence for GSW victim. 44 year old female patient. GSW to upper right forearm", "F",
+		GunShotWoundReport report6 = new GunShotWoundReport("Medic 11 LCEMS", "09-01-2019", "1006", "Medic 21 dispatched to residence for GSW victim. 44 year old female patient. GSW to upper right forearm", "F",
 			"44", "60/40", "56", "80%", "14", "3",
-			"n/a", "right forearm", "1", true, "glock", false);
+			"n/a", "right forearm", "1", true, "Shot Gun", false);
 		gswRepo.save(report6);
 		System.out.println("RUNNING INITIALIZER");
 
